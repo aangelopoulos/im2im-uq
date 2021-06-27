[nonworking, but towards correct normalization]

core/models/add_uncertainty.py
@@ -8,6 +8,7 @@ from core.models.finallayers.residual_magnitude_layer import ResidualMagnitudeLa
 from core.models.finallayers.softmax_layer import SoftmaxLayer, softmax_loss_fn, softmax_nested_sets_from_output
 from core.models.trunks.wnet import WNet
 from core.models.trunks.unet import UNet
+from core.utils import standard_to_minmax
 import json
 
 class ModelWithUncertainty(nn.Module):
@@ -27,14 +28,17 @@ class ModelWithUncertainty(nn.Module):
   def loss_fn(self, pred, target):
     return self.in_train_loss_fn(pred,target,self.params) 
 
+  # Always outputs [0,1] valued nested sets
   def nested_sets_from_output(self, output, lam=None):
     lower_edge, prediction, upper_edge = self.in_nested_sets_from_output_fn(self, output, lam)
     upper_edge = torch.maximum(upper_edge, output[:,1,:,:,:] + 1e-6) # set a lower bound on the size.
     lower_edge = torch.minimum(lower_edge, output[:,1,:,:,:] - 1e-6)
 
-    lower_edge = (lower_edge * self.params["output_max"]) + self.params["output_min"]
-    prediction = (prediction * self.params["output_max"]) + self.params["output_min"]
-    upper_edge = (upper_edge * self.params["output_max"]) + self.params["output_min"]
+    # IF MEAN STD NORM, HAVE TO FIX THAT
+    if self.params["output_normalization"] == "standard":
+      lower_edge = standard_to_minmax(lower_edge,self.params,output_bool=True) 
+      prediction = standard_to_minmax(prediction,self.params,output_bool=True) 
+      upper_edge = standard_to_minmax(upper_edge,self.params,output_bool=True) 
 
     return lower_edge, prediction, upper_edge 
 

core/scripts/eval.py
@@ -7,12 +7,13 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from torch.utils.data import TensorDataset
 from core.calibration.calibrate_model import get_rcps_loss_fn, get_rcps_metrics_from_outputs
+from core.utils import standard_to_minmax
 import wandb
 import pdb
 
 def transform_output(x):
-  x = x - x.min()
-  x = x/x.max() 
+  x[x>1] = 1
+
   x = np.maximum(0,np.minimum(255*x.cpu().squeeze(), 255))
   if len(x.shape) == 3:
     x = x.permute(1,2,0)
@@ -21,17 +22,31 @@ def transform_output(x):
 def get_images(model,
                val_dataset,
                device,
-               idx_iterator):
+               idx_iterator,
+               config):
   with torch.no_grad():
+    model = model.to(device)
     lam = None
     if model.lhat == None:
-      lam = 1.0
-    examples_input = [wandb.Image(transform_output(val_dataset[img_idx][0])) for img_idx in idx_iterator]
-    examples_ground_truth = [wandb.Image(transform_output(val_dataset[img_idx][1])) for img_idx in idx_iterator]
+      if config["uncertainty_type"] != "softmax":
+        lam = 1.0
+      else:
+        lam = 0.99
+
+    if config["input_normalization"] == "standard":
+      examples_input = [wandb.Image(transform_output(standard_to_minmax(val_dataset[img_idx][0],config,output_bool=False))) for img_idx in idx_iterator]
+    else:
+      examples_input = [wandb.Image(transform_output(val_dataset[img_idx][0])) for img_idx in idx_iterator]
     examples_output = [model.nested_sets((val_dataset[img_idx][0].unsqueeze(0).to(device),),lam=lam) for img_idx in idx_iterator]
-    examples_lower_edge = [wandb.Image(transform_output(example[0])) for example in examples_output]
-    examples_prediction = [wandb.Image(transform_output(example[1])) for example in examples_output]
-    examples_upper_edge = [wandb.Image(transform_output(example[2])) for example in examples_output]
+    min_val = min([example[0].min().item() for example in examples_output]) 
+    max_val = max([example[2].max().item() for example in examples_output]) 
+    examples_lower_edge = [wandb.Image(transform_output((example[0]-min_val)/max_val)) for example in examples_output]
+    examples_prediction = [wandb.Image(transform_output((example[1]-min_val)/max_val)) for example in examples_output]
+    examples_upper_edge = [wandb.Image(transform_output((example[2]-min_val)/max_val)) for example in examples_output]
+    if config["output_normalization"] == "standard":
+      examples_ground_truth = [wandb.Image(transform_output((standard_to_minmax(val_dataset[img_idx][1],config,output_bool=True)-min_val)/max_val)) for img_idx in idx_iterator]
+    else:
+      examples_ground_truth = [wandb.Image(transform_output((val_dataset[img_idx][1]-min_val)/max_val)) for img_idx in idx_iterator]
     return examples_input, examples_lower_edge, examples_prediction, examples_upper_edge, examples_ground_truth
 
 def eval_set_metrics(model, dataset, config):
@@ -41,6 +56,8 @@ def eval_set_metrics(model, dataset, config):
     rcps_loss_fn = get_rcps_loss_fn(config)
     model = model.to(device)
     labels = torch.cat([x[1].unsqueeze(0).to(device) for x in dataset], dim=0)
+    if config["output_normalization"] == "standard":
+      labels = standard_to_minmax(labels,config,output_bool=True)
     outputs_shape = list(model(dataset[0][0].unsqueeze(0).to(device)).shape)
     outputs_shape[0] = len(dataset)
     outputs = torch.zeros(tuple(outputs_shape),device=device)

core/scripts/train.py
@@ -31,7 +31,8 @@ def run_validation(net,
                    val_dataset,
                    device,
                    global_step,
-                   epoch):
+                   epoch,
+                   config):
   with torch.no_grad():
     net.eval()
     val_loss = eval_net(net, val_loader, device)
@@ -42,7 +43,8 @@ def run_validation(net,
       examples_input, examples_lower_edge, examples_prediction, examples_upper_edge, examples_ground_truth = get_images(net,
                                                                                                                         val_dataset,
                                                                                                                         device,
-                                                                                                                        list(range(5)))
+                                                                                                                        list(range(5)),
+                                                                                                                        config)
       # Log everything
       wandb.log({"epoch": epoch, "iter":global_step, "examples_input": examples_input})
       wandb.log({"epoch": epoch, "iter":global_step, "Lower edge": examples_lower_edge})
@@ -119,7 +121,8 @@ def train_net(net,
                    val_dataset,
                    device,
                    global_step,
-                   0)
+                   0,
+                   config)
 
     for epoch in range(starting_epoch,epochs):
         net.train()
@@ -139,7 +142,7 @@ def train_net(net,
             optimizer.zero_grad()
             loss.backward()
             loss.retain_grad()
-            nn.utils.clip_grad_value_(net.parameters(), 0.1)
+            #nn.utils.clip_grad_value_(net.parameters(), 0.1)
             optimizer.step()
 
             global_step += 1
@@ -156,7 +159,8 @@ def train_net(net,
                              val_dataset,
                              device,
                              global_step,
-                             epoch)
+                             epoch,
+                             config)
               #scheduler.step(val_loss)
 
           if (epoch+1) % checkpoint_every == 0: