242be36d43b082a3082851e1d3f552da8961c2aa
Amit Kohli <apkohli@fiona.abc.berkeley.edu>
2021-10-07 11:14:19 -0700

    working temca dataset

--- core/datasets/temca/TEMCADataset.py
+++ core/datasets/temca/TEMCADataset.py
@@ -17,48 +17,44 @@ mpl.rcParams['figure.dpi'] = 500
 
 class TEMCADataset(IterableDataset):
     # path should be absolute, num instances is an int or 'all', normalize can be None, 'standard', or 'min-max'
-    def __init__(self, path, input_patch_size, upsampling, num_patches, buffer_size=10, normalize=None):
+    def __init__(self, path, patch_size, downsampling, num_imgs, buffer_size, normalize):
         print('loading dataset from : ' + path  + '...')
         
         # load in flags
         self.path = path
-        self.output_size = [input_patch_size[0]*upsampling[0], input_patch_size[1]*upsampling[1]]
-        self.upsampling = upsampling
-        self.num_patches = num_patches
-        self.buffer_size = buffer_size        
-        
+        self.output_size = patch_size #[input_patch_size[0]*upsampling[0], input_patch_size[1]*upsampling[1]]
+        self.downsampling = downsampling
+        self.num_imgs = num_imgs
+        self.buffer_size = buffer_size                
+        self.img_index = 0  
+        self.normalize = normalize
+
         # collect all the imgs as filepaths
         self.img_paths = glob(path + '**/*.png')
-        print(len(self.img_paths))
         random.shuffle(self.img_paths)
-        self.img_index = 0 
- 
+        self.img_paths = self.img_paths[0:num_imgs]
+        print(len(self.img_paths))
+        
         #read in the first buffer
         self.patch_buffer = []
         self.get_buffer()
-        
 
     def get_buffer(self):
-        if self.img_index + self.buffer_size > len(self.img_paths) - 1:
-            if len(self.img_paths) - 1 - self.img_index > 0 :
-                end_point = len(self.img_paths) - 1 - img_index + img_index
+        if self.img_index + self.buffer_size > len(self.img_paths):
+            if len(self.img_paths) -  self.img_index > 0 :
+                end_point = len(self.img_paths) - self.img_index
             else:
-                self.img_index = 0
-                end_point = self.buffer_size
+                self.img_index = -1
+                return
         else:
             end_point = self.img_index + self.buffer_size
         
-        #img_buffer_paths = self.img_paths[self.img_index:end_point]
-        img_buffer_paths = self.img_paths
+        img_buffer_paths = self.img_paths[self.img_index:end_point]
         for img_path in img_buffer_paths:
-            print(img_path)
             img = imageio.imread(img_path)
-            if img[np.where(img==0)].size > 0.85*(img.shape[0] * img.shape[1]):
-                print('NOOOO GET OUUUUUT')
-                os.remove(img_path)
-            #print(img[np.where(img==0)].size)
-            #self.extract_patches(imageio.imread(img_path))   
-      
+            self.extract_patches(imageio.imread(img_path))   
+        random.shuffle(self.patch_buffer)
+        self.img_index = end_point
 
     def extract_patches(self, img):
         row_slices = img.shape[0] // self.output_size[0]
@@ -69,36 +65,41 @@ class TEMCADataset(IterableDataset):
                   r_end = r_start + self.output_size[0] 
                   c_start = c*self.output_size[1]
                   c_end = c_start + self.output_size[1]
-                  self.patch_buffer += [img[r_start:r_end, c_start:c_end]]
-        pdb.set_trace()
+                  patch = img[r_start:r_end, c_start:c_end]
+                  if patch[np.where(patch==0)].size < 0.85*(patch.shape[0] * patch.shape[1]):
+                      self.patch_buffer += [patch]
+                  else:
+                      print('NOOOO') 
                 
+
 
 
     #def __len__(self):
         
     #    return sum(self.num_patches)
 
-    def __iter__(self, idx):
-        gt = self.get_patch(idx).astype(np.float32)#.values.astype(np.float32)
-        #if normalize:
-        
-        return torch.from_numpy(gt[None,0::self.scale[0], 0::self.scale[1]]), torch.from_numpy(gt[None, :, :])        
-
+    def __iter__(self):
+        while self.img_index != -1:
+            if len(self.patch_buffer) == 0:
+                self.get_buffer()
+            if len(self.patch_buffer) > 0:
+                gt = self.patch_buffer.pop().astype(np.float32)#.values.astype(np.float32)
+                if self.normalize == '01':
+                    gt = gt/255.0
+                if self.normalize == '-11':
+                    gt = 2*(gt/255.0 - 0.5)
+                yield torch.from_numpy(gt[None,0::self.downsampling[0], 0::self.downsampling[1]]), torch.from_numpy(gt[None, :, :])
 
 if __name__ == "__main__":
         
     # Testing the dataset
-    dataset = TEMCADataset('/local/amit/temca_data/', input_patch_size=[128, 128], upsampling=[2,2], num_patches='all')
-    #pdb.set_trace()
-    loader = DataLoader(dataset, batch_size=1, drop_last=True, num_workers=2)    
-    #print(len(dataset))
-    print(len(loader)) 
-    prev_time = time.time()
+    dataset = TEMCADataset('/local/amit/more_data/', patch_size=[2048, 2048], downsampling=[4,4], num_imgs=10, buffer_size=15, normalize='-11') 
+    loader = DataLoader(dataset, batch_size=16, drop_last=False, num_workers=0)    
+    img = next(iter(dataset))
+  
+    samp = 0
     for idx, sample in enumerate(loader):
-        curr_time = time.time()
-        print(curr_time-prev_time)
-        prev_time = curr_time
-        #if d.max() > max_val:
-        #max_val = d.max()
-
+        print(samp)
+        samp += sample[1].shape[0]
+    print(samp)
     pdb.set_trace()    
