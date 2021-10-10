69330c10ff0cde26ee3c2a6f40f673cf2f8e19b2
Anastasios Nikolas Angelopoulos <angelopoulos@fiona.abc.berkeley.edu>
2021-10-10 15:58:42 -0700

    [checkpoint]

--- sshell.sh
+++ sshell.sh
@@ -1,3 +1,3 @@
-srun --gpus-per-task=3 --time=0 --cpus-per-task=1 --mem-per-cpu=20G  --qos=abc_normal --partition=abc --account=co_abc  --nodes=1 --pty /bin/bash
+srun --gpus-per-task=2 --time=0 --cpus-per-task=1 --mem-per-cpu=20G  --qos=abc_normal --partition=abc --account=co_abc  --nodes=1 --pty /bin/bash
 module load cuda/10.0
 module load cudnn/7.5
